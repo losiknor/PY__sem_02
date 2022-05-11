b5521fd6ded87e6331687fc2c7d8d1319061c238
Larina <Losiknor@gmail.com>
2022-05-11 15:07:56 +0700

    Решили четвёртую задачу

--- zad_4.py
+++ zad_4.py
@@ -1,1 +1,28 @@
 # Показать последнюю цифру трёхзначного числа
+# digit = input('--->  ')
+# print(digit[-1])
+
+# вариант записи того же решения
+# digit = input('--->  ')[-1]
+# print(digit)
+
+# с числовым форматом
+# number = int(input('Введи трехзначное число: '))
+# if 99 < number < 1000 :
+#     lastDigit = number % 10
+#     print(f'Поcледняя цифра {lastDigit}')
+# else:
+#     print('Не трехзначное')
+
+# то же самое с проверкой в цикле  while  корректности ввода
+while True:
+    number = int(input('Введи трехзначное число: '))
+    if 99 < number < 1000 :
+        print(f'Поcледняя цифра ---> {number % 10}')
+        break
+    else:
+        print('Не трехзначное')
+
+# генерация рандомных чисел в Python  - нужно импортировать библиотеку
+# import random
+# number = random.randint(101, 999)
