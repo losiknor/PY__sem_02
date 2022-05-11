c7b530aefc860681b489f56c1a967aafe05f52f9
Larina <Losiknor@gmail.com>
2022-05-11 14:17:37 +0700

    Решили вторую задачу_

--- zad_2.py
+++ zad_2.py
@@ -10,9 +10,15 @@
 
 
 # Второй способ
-WeekDays = ["Monday", 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
-number = int(input('Enter the number: ')) - 1
-if number <= 0 or number >= len(WeekDays):
-    print("Not")
-else:
-    print(WeekDays[number])
+# WeekDays = ["Monday", 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+# number = int(input('Enter the number: ')) - 1
+# if number <= 0 or number >= len(WeekDays):
+#     print("Not")
+# else:
+#     print(WeekDays[number])
+
+
+# Третий способ - не понятно
+Week = [[1, 2, 3, 4, 5, 6, 7],['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturnay', 'Sunday']]
+DayOfWeek = int(input('Введите номер дня недели: '))
+print(Week[1][Week[0].index(DayOfWeek)])
